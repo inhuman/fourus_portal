1b290bb353fa148be37a627e91aa1087b0d620cd
iD <reg.backup@mail.ru>
2013-10-02 16:35:36 +0400

    добавил файл для настроек и инструкций

--- sidebar.php
+++ sidebar.php
@@ -2,3 +2,8 @@
 echo '<br><br><a href="create_lic"><button class="btn btn-large btn-block btn-primary" type="button">Лицензии</button></a>';
 echo '<br><a href="rides"><button class="btn btn-large btn-block" type="button">Райды</button></a>';
 echo '<br><a href="validity"><button class="btn btn-large btn-block" type="button">Сроки</button></a>';
+
+
+echo '<br><a href="setings"><button class="btn btn-large btn-block" type="button">Сроки</button></a>';
+echo '<br><a href="wiki"><button class="btn btn-large btn-block" type="button">Сроки</button></a>';
+
