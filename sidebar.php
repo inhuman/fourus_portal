c5999af8084e834bdd6f004969713675b8039eb6
iD <reg.backup@mail.ru>
2013-10-02 16:39:21 +0400

    добавил файл для аттракционов

--- sidebar.php
+++ sidebar.php
@@ -1,9 +1,11 @@
 <?php
 echo '<br><br><a href="create_lic"><button class="btn btn-large btn-block btn-primary" type="button">Лицензии</button></a>';
+echo '<br><br><a href="attractions"><button class="btn btn-large btn-block btn-primary" type="button">Аттракционы</button></a>';
+
 echo '<br><a href="rides"><button class="btn btn-large btn-block" type="button">Райды</button></a>';
 echo '<br><a href="validity"><button class="btn btn-large btn-block" type="button">Сроки</button></a>';
 
 
-echo '<br><a href="setings"><button class="btn btn-large btn-block" type="button">Сроки</button></a>';
-echo '<br><a href="wiki"><button class="btn btn-large btn-block" type="button">Сроки</button></a>';
+echo '<br><a href="setings"><button class="btn btn-large btn-block" type="button">Настройки</button></a>';
+echo '<br><a href="wiki"><button class="btn btn-large btn-block" type="button">Инструкции</button></a>';
 
