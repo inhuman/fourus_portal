85786874a9ddf02d97c23c2d165295f03d4c697e
iD <reg.backup@mail.ru>
2013-10-02 15:50:23 +0400

    добавил twitter bootstrap

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
+<!DOCTYPE html>
+<html lang="ru">
 <html>
 <head>
     <title>Fourus portal</title>
-    <link rel="stylesheet" type="text/css" href="style/styles.css" />
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
 </head>
 <body>
+
+<div class="container-fluid">
+    <div class="row-fluid">
+        <div class="span2">
+            <!--Sidebar content-->
+            sidebar
+        </div>
+        <div class="span10">
+            <!--Body content-->
+            body
+        </div>
+    </div>
+</div>
+
 <?php
 
 echo "<h1>Fourus portal</h1>";
