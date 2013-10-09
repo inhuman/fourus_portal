9df3d74c5f2229f539de1f49751ffc5967af1625
iD <reg.backup@mail.ru>
2013-10-09 14:47:15 +0400

    добавил иконки эффектов в гит добавил иконки к форме "установить эффекты"

--- card/RideEditCard.php
+++ card/RideEditCard.php
@@ -11,12 +11,12 @@ echo '<div class="container">';
         echo '<form action="hWriteRideEffectsToDB.php">';
         echo '<fieldset>';
         echo '<label><b>Установить эффекты</b></label>';
-        echo '<input type="checkbox"> Ветер';
-        echo '<br><input type="checkbox" name="water" value="true"> Брызги';
-        echo '<br><input type="checkbox" name="boobles" value="true"> Пузыри';
-        echo '<br><input type="checkbox" name="lighting" value="true"> Молния';
-        echo '<br><input type="checkbox" name="vibro" value="true"> Вибро';
-        echo '<br><input type="checkbox" name="mouse" value="true"> Мыши';
+        echo '<img src="img/wind.png"> <input type="checkbox"> Ветер';
+        echo '<br><img src="img/water.png"> <input type="checkbox" name="water" value="true"> Брызги';
+        echo '<br><img src="img/boobles.png"> <input type="checkbox" name="boobles" value="true"> Пузыри';
+        echo '<br><img src="img/lighting.png"> <input type="checkbox" name="lighting" value="true"> Молния';
+        echo '<br><img src="img/vibro.png"> <input type="checkbox" name="vibro" value="true"> Вибро';
+        echo '<br><img src="img/mouse.png"> <input type="checkbox" name="mouse" value="true"> Мыши';
 
 
         echo '<br><br><button type="submit" class="btn">Сохранить</button>';
