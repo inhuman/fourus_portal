952628d4b95d18d6b48de855b1713937f55b9772
iD <reg.backup@mail.ru>
2013-11-05 17:25:40 +0400

    почти работающий autolic.ini сборщик

--- class/FactoryLicBlueprint.php
+++ class/FactoryLicBlueprint.php
@@ -1,6 +1,8 @@
 <?php
 require_once __DIR__."/LicBlueprint.php";
 require_once __DIR__."/PDOConfig.php";
+require_once __DIR__."/FactoryRide.php";
+require_once __DIR__."/FactoryAttraction.php";
 
 class FactoryLicBlueprint extends LicBlueprint {
 
@@ -8,6 +10,7 @@ class FactoryLicBlueprint extends LicBlueprint {
     {
         parent::__construct($attrId, $rideId, $dateTo, $volume, $licOnly);
         $this->addLicBlueprintToQueue();
+        $this->createAutoLicIni();
     }
 
     private function addLicBlueprintToQueue()
@@ -31,9 +34,41 @@ class FactoryLicBlueprint extends LicBlueprint {
         $stmt->closeCursor();
     }
 
-    private function  createIniSection(){}
-    private function createAutoLicIni(){}
+    private function  createAutoLicIni()
+    {
+        $ride        = FactoryRide::findRide($this->getRideId());
+        $attraction  = FactoryAttraction::findOne($this->getAttrId());
+
+        $ride->
+
+        var_dump($ride);
+        var_dump($attraction);
+        $string1 = "name=$ride->getRideName";
+        $string2 = "path=$ride->getFileName";
+
+        $dateArr = explode('-',$this->getDateTo());
+        $string3 = "date=$dateArr[2]{RIGHT}$dateArr[1]{RIGHT}$dateArr[0]";
+
+        $string4 = "id=$attraction->getSerialId()";
+
+        if($this->getLicOnly() == true){$string5 = "lic=''";}
+        else{$string5 = "lic='{SPACE}'";}
+
+        $vol = 100 - $this->getVolume();
+        $string6 = "volume={DOWN $vol}";
+
+
+        echo "<br>".$string1;
+        echo "<br>".$string2;
+        echo "<br>".$string3;
+        echo "<br>".$string4;
+        echo "<br>".$string5;
+        echo "<br>".$string6;
+    }
+
+
+
 }
 
 
-$ew = new FactoryLicBlueprint(114,4,'2013-12-30',80,1);
+$ew = new FactoryLicBlueprint(114,9,'2013-12-30',80,1);
